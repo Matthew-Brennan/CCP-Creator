a730afa0f64bf8f6b31dfb95e183e164215575da
mbrennan-eastbay <32656444+Matthew-Brennan@users.noreply.github.com>
2024-06-19 16:56:36 -0400

    added the controller

--- force-app/main/default/lwc/cCPCreator/cCPCreator.js
+++ force-app/main/default/lwc/cCPCreator/cCPCreator.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
 
-export default class CCPCreator extends LightningElement {}
+// Specify the fields to fetch
+const FIELDS = [
+    {label: 'CCP Type', fieldName: 'CCP'},
+    {label: 'Client Hourly Rate', fieldName: 'StandardHourlyRate'},
+    {label: '# of Hours', fieldName: 'ofHours'},
+    {label: 'Total Without Discount', fieldName: 'CCPTotatlWithoutDiscount'},
+    {label: 'Discount Rate', fieldName: 'CCPDiscountRate' },
+    {label: 'Discount Amount', fieldName: 'CCPDiscountAmount'},
+    {label: 'Client Price with Discount', fieldName: 'CCPClientPriceWithDiscount'}
+];
+
+export default class DisplayCCPFields extends LightningElement {
+    @api recordId;
+
+    @wire(getRecord, { oppID: '$recordId', fields: FIELDS })
+
+}
